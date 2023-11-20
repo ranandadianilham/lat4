17e51916d58e755ba425df76efe916edd6675d7c
Rananda Dian Ilham <=>
2023-11-20 21:06:12 +0700

    latest bank account

--- src/main/java/com/example/demo/Controllers/BankAccountController.java
+++ src/main/java/com/example/demo/Controllers/BankAccountController.java
@@ -7,31 +7,42 @@ import com.example.demo.repository.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/idstar")
+@RequestMapping("/v1/idstar/bank")
 public class BankAccountController {
 
     @Autowired
     private BankAccountRepository bankAccountRepository;
 
-    @GetMapping("/accounts")
+    @GetMapping("/list")
     public ResponseEntity<BaseResponse> getAll() {
         List<Bank_Account> notes = bankAccountRepository.findAll();
         return new ResponseEntity<>(new BaseResponse(200, "success", notes), HttpStatus.OK);
     }
 
-    @GetMapping("/note/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<BaseResponse> getById(@PathVariable Long id) {
         Optional<Bank_Account> note = bankAccountRepository.findById(id);
         return new ResponseEntity<>(new BaseResponse(200, "success", note), HttpStatus.OK);
+    }
+    @PostMapping("/save")
+    public ResponseEntity<BaseResponse> save() {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> editById(@PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponse> deleteById(@PathVariable Long id) {
+        return null;
     }
 
 
